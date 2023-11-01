39c6f36832a35fb38820bf26b894c7b31cb9722e
Harry-Neal <ppyhn1@nottingham.ac.uk>
2023-11-01 16:34:31 +0000

    testing of code

---
+++ Ploting.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('C:\Users\ppyhn1\OneDrive - The University of Nottingham\Year 4\Project\Chaos-in-Many-a-Many-Body-System-Out-of-Equilibrium\output\Av.dat')
