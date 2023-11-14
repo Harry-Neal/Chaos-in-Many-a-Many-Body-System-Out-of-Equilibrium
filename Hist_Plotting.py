225c347987c035345e6a5c6e91febf8a5ade4ded
Harry-Neal <ppyhn1@nottingham.ac.uk>
2023-11-14 18:08:33 +0000

    hist plotted

--- Hist_Plotting.py
+++ Hist_Plotting.py
@@ -6,11 +6,14 @@ data1 = np.loadtxt(Analytic_dat)
 x1 = data1[:,0]
 y1 = data1[:,1]
 
-Actual_dat = "data/Hist.dat"
+Actual_dat = "Hist.dat"
 data2 = np.loadtxt(Actual_dat)
 x2 = data2[:,0]
 y2 = data2[:,1]
 
-plt.plot(x1,y1,color='r')
-plt.bar(x2,y2,width=0.01,color='b')
+plt.plot(x1,y1,color='r',label = "Analytical")
+plt.bar(x2,y2,width=0.01,color='white',edgecolor='b',label = "simulation")
+plt.xlabel('Mean Energy Density',fontsize=20)
+plt.ylabel('N points',fontsize=20)
+plt.legend(fontsize=20)
 plt.show()
