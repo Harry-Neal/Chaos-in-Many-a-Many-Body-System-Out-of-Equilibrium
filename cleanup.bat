3d8018c5751489453b72e82ed8bc6b3ce7166b69
Harry-Neal <148877711+Harry-Neal@users.noreply.github.com>
2024-05-24 15:29:45 +0100

    Delete cleanup.bat

--- cleanup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-MOVE C:\Users\ppyhn1\Desktop\Chaos-in-Many-a-Many-Body-System-Out-of-Equilibrium\*.dat C:\Users\ppyhn1\Desktop\Chaos-in-Many-a-Many-Body-System-Out-of-Equilibrium\data"
-PAUSE
-DEL C:\Users\ppyhn1\Desktop\Chaos-in-Many-a-Many-Body-System-Out-of-Equilibrium\*.exe"
